ed7959a81ce8e6077fb5f7341c881a10d11ab50d
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2018-10-23 19:51:41 -0700

    fixed tab heresy

--- pybuff/player.py
+++ pybuff/player.py
@@ -1,64 +1,64 @@
 
 class Player:
-	def __init__(self, btag, platform, soup):
-		self.btag = btag
-		self.platform = platform
-		self.soup = soup
+    def __init__(self, btag, platform, soup):
+        self.btag = btag
+        self.platform = platform
+        self.soup = soup
 
-	def get_sr(self):
-		""" Get the SR of this player.
-			If SR isn't displayed on their profile, returns none.
+    def get_sr(self):
+        """ Get the SR of this player.
+            If SR isn't displayed on their profile, returns none.
 
-			:rtype:
-				integer | None
-			:returns:
-				The SR of this player.
-		"""
+            :rtype:
+                integer | None
+            :returns:
+                The SR of this player.
+        """
 
-		sr_div = self.soup.find(class_='player-skill-rating')
-		if sr_div:
-			return int(sr_div.contents[0])
+        sr_div = self.soup.find(class_='player-skill-rating')
+        if sr_div:
+            return int(sr_div.contents[0])
 
-	def get_roles(self):
-		""" Get the amount of wins on each role for this player.
+    def get_roles(self):
+        """ Get the amount of wins on each role for this player.
 
-			:rtype:
-				list of tuples
-			:returns:
-				A list with each role as a tuple in this format:
+            :rtype:
+                list of tuples
+            :returns:
+                A list with each role as a tuple in this format:
 
-					(str: name, int: wins)
+                    (str: name, int: wins)
 
-					For example:
-					('Tank', 112)
-		"""
+                    For example:
+                    ('Tank', 112)
+        """
 
-		roles_container = self.soup.find(class_='table-data')
-		if not roles_container:
-			return [('', 0)]
+        roles_container = self.soup.find(class_='table-data')
+        if not roles_container:
+            return [('', 0)]
 
-		roles = roles_container.contents[1].contents
+        roles = roles_container.contents[1].contents
 
-		def create_role(div):
-			""" Make a role tuple from a role div """
-			wins = int(div.contents[2].attrs['data-value'])
-			role_name_div = div.contents[1]
-			role_name = role_name_div.contents[0].string
-			return (role_name, wins)
-		
-		return [create_role(div) for div in roles]
+        def create_role(div):
+            """ Make a role tuple from a role div """
+            wins = int(div.contents[2].attrs['data-value'])
+            role_name_div = div.contents[1]
+            role_name = role_name_div.contents[0].string
+            return (role_name, wins)
 
-	def get_role(self):
-		""" Get the main role of this player.
-			This assumes that the role with the most wins is their main.
+        return [create_role(div) for div in roles]
 
-			:rtype:
-				tuple
-			:returns:
-				A tuple with the name and win count of their main role.
-		"""
+    def get_role(self):
+        """ Get the main role of this player.
+            This assumes that the role with the most wins is their main.
 
-		return self.get_roles()[0]
+            :rtype:
+                tuple
+            :returns:
+                A tuple with the name and win count of their main role.
+        """
 
-	def __str__(self):
-		return self.btag
+        return self.get_roles()[0]
+
+    def __str__(self):
+        return self.btag
--- tests/get_player_test.py
+++ tests/get_player_test.py
@@ -8,21 +8,21 @@ from pybuff.grabber import BadBattletag
 
 @pytest.mark.asyncio
 async def test_get_player_pc():
-	player = await get_player('Tydra#11863')
-	assert str(player) == 'Tydra#11863'
+    player = await get_player('Tydra#11863')
+    assert str(player) == 'Tydra#11863'
 
 @pytest.mark.asyncio
 async def test_get_player_console():
-	for profile in [('Carried Reaper', 'xbl'), ('Im_Roadhog', 'psn')]:
-		player = await get_player(profile[0], platform=profile[1])
-		assert str(player) == profile[0]
+    for profile in [('Carried Reaper', 'xbl'), ('Im_Roadhog', 'psn')]:
+        player = await get_player(profile[0], platform=profile[1])
+        assert str(player) == profile[0]
 
 @pytest.mark.asyncio
 async def test_get_player_invalid_battletag():
-	with pytest.raises(BadBattletag):
-		await get_player('ogdog')
+    with pytest.raises(BadBattletag):
+        await get_player('ogdog')
 
 @pytest.mark.asyncio
 async def test_get_player_nonexistent():
-	with pytest.raises(BadBattletag):
-		await get_player('Ogdog#1234')
+    with pytest.raises(BadBattletag):
+        await get_player('Ogdog#1234')
