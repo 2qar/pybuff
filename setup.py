cbdb1e8233d04ceab6d5b442f07401f6ac3934e9
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2018-10-23 21:52:31 -0700

    packaging stuff

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md") as file:
-	long_desc = file.read()
+        long_desc = file.read()
 
 setup(
-	name="pybuff",
-	author="BigHeadGeorge",
-	author_email="tucker1014@hotmail.com",
-	url="https://github.com/BigHeadGeorge/overbuff.py",
-	version="0.0.1",
-	description="A scraper for grabbing info from Overbuff.",
-	long_description=long_desc,
-	long_description_content_type="text/markdown",
-	packages=find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operation System :: OS Independent"
-	]
+        name="pybuff",
+        author="BigHeadGeorge",
+        url="https://github.com/BigHeadGeorge/overbuff.py",
+        version="0.0.1",
+        packages=['pybuff'],
+        zip_safe=True,
+        description="A scraper for grabbing info from Overbuff.",
+        long_description=long_desc,
+        long_description_content_type="text/markdown",
+        classifiers=[
+                "Development Status :: 2 - Pre-Alpha",
+                "License :: OSI Approved :: MIT License",
+                "Programming Language :: Python :: 3",
+                "Operating System :: OS Independent",
+                "Intended Audience :: Developers",
+                "Natural Language :: English",
+                "Topic :: Internet",
+                "Topic :: Software Development :: Libraries",
+                "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
 )
